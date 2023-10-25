10437eac7d6ffca8cdfb424bc170fa033cd9ca39
Cleiton dos Santos Cunha <102325156+cleitoncunha04@users.noreply.github.com>
2023-10-24 21:59:05 -0300

    Testes e mais testes

--- tela-login.php
+++ tela-login.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/css/estilo-t-login.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/estilo-t-login.css"/>
     <title>myProjectManager</title>
   </head>
   <body>
